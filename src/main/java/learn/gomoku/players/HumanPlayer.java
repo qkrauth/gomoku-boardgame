a32944ca2ef5d8302cbf03d9b0345d79bfd4b261
Quinten Krauth <quintenkrauth19@gmail.com>
2023-09-21 18:13:01 -0400

    working on gomoku

--- src/main/java/learn/gomoku/players/HumanPlayer.java
+++ src/main/java/learn/gomoku/players/HumanPlayer.java
@@ -1,4 +1,25 @@
 package learn.gomoku.players;
 
-public class HumanPlayer {
+import learn.gomoku.game.Stone;
+
+import java.util.List;
+
+public class HumanPlayer implements Player {
+
+    private final String name;
+
+    public HumanPlayer(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Stone generateMove(List<Stone> previousMoves) {
+        return null;
+        // HumanPlayer should be asked the coords of where they want to move
+    }
 }
--- src/main/java/learn/gomoku/players/NPCPlayer.java
+++ src/main/java/learn/gomoku/players/NPCPlayer.java
@@ -1,4 +1,43 @@
 package learn.gomoku.players;
 
-public class NPCPlayer {
+import learn.gomoku.game.Gomoku;
+import learn.gomoku.game.Stone;
+
+import java.util.List;
+import java.util.Random;
+
+public class NPCPlayer implements Player {
+
+    private static String[] names = {
+            "Andrew", "Benson", "Carl", "Dennis", "Emily", "Fred",
+            "Gina", "Holly", "Ines", "Jay", "Kingsley", "Loma", "Magnus"
+    };
+
+    private final Random random = new Random();
+    private final String name;
+
+    public NPCPlayer() {
+        name = String.format("%s",
+                names[random.nextInt(names.length)]
+        );
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Stone generateMove(List<Stone> previousMoves) {
+
+        boolean isBlack = true;
+        if (previousMoves != null && !previousMoves.isEmpty()) {
+            Stone lastMove = previousMoves.get(previousMoves.size() - 1);
+            isBlack = !lastMove.isBlack();
+        }
+        return new Stone(
+                random.nextInt(Gomoku.WIDTH),
+                random.nextInt(Gomoku.WIDTH),
+                isBlack);
+    }
 }
